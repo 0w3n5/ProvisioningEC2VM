2e5b3c52ccfee6e59146de3c4f2b1067734ab77d
Owen Sweet <67649781+0w3n5@users.noreply.github.com>
2025-07-10 23:07:03 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@
 # aws instance web is EC2
 # public_ip calls the ip variable
 
-output = "instance_public_ip" {
-  value = aws_instance.web.public_ip
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip         # After creation, show the instance's public IP in terminal
 }
